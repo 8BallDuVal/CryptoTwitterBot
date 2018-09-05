b79d5818bea1280d8e31c639b8a02f4064b4064a
Daniel <39527881+8BallDuVal@users.noreply.github.com>
2018-09-05 16:00:57 -0400

    Add files via upload

---
+++ API_scrape.py
@@ -0,0 +1,57 @@
+import requests 
+import tweepy
+
+r = requests.get('https://api.coinmarketcap.com/v1/ticker/?limit=11')
+
+prices = []
+symbols = []
+change = []
+market_cap = []
+
+for coin in r.json():
+    ticker = '#'+coin.get("symbol", "Cryptocurrency Name Note Provided")
+    symbols.append(ticker)
+    
+    price = float(coin.get("price_usd", "US$ Price not provided"))
+    test = round(price, 3)
+    prices.append(test)
+    
+    percent = '('+ coin.get("percent_change_1h", "Percent Change not provided")+'%)'
+    change.append(percent)
+    
+
+#print(symbols)
+#print(prices)
+#print(change)
+pricedata = []
+for i in range(len(symbols)):
+    pricedata.append(str(symbols[i] +': '+ str(prices[i]) +' '+ change[i]+'\n'))
+
+final_pricedata = (pricedata[0] + 
+                  pricedata[1] +
+                  pricedata[2] +
+                  pricedata[3] +
+                  pricedata[4] +
+                  pricedata[5] +
+                  pricedata[6] +
+                  pricedata[7] +
+                  pricedata[8] +
+                  pricedata[9] +
+                  pricedata[10])
+
+
+    
+# consumer_key = 'JHujYZhB3EZKOUG8BeVC9dXpF'
+# consumer_secret = 'Q9PJcESP9AQLLAT8vn5JioyXY1Yf2QWR3pIX3CrrZAl97qlEMc'
+# access_token = '1006253589763969024-ecScdz9goBVFNBSWrZXTOibylLIE1k'
+# access_token_secret = 'lEmflohmqtcBSzysVP0FgghKUvkLUIrkhpmdVgOqLqqxT'
+# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+# auth.set_access_token(access_token, access_token_secret)
+# api = tweepy.API(auth)
+#   
+# api.update_status(final_pricedata)
+
+print(final_pricedata)
+
+
+
