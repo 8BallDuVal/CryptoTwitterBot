6ee2db82c6b2f41aac55fff4a41ff4205093e72c
Daniel <39527881+8BallDuVal@users.noreply.github.com>
2021-06-17 20:07:36 -0400

    Update API_scrape.py
    Made several improvements.

--- API_scrape.py
+++ API_scrape.py
@@ -6,12 +6,12 @@ import json
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 parameters = {
   'start':'1',
-  'limit':'11',
+  'limit':'12',
   'convert':'USD'
 }
 headers = {
   'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': '',                                                  # info from coinmarketcap.com/api/
+  'X-CMC_PRO_API_KEY': '',
 }
 
 session = Session()
@@ -28,21 +28,27 @@ try:
             for item in value:
                 for key_1,value_1 in item.items():
                     if key_1 == 'quote':
-                        price_rounded = round(float(value_1['USD']['price']), 3)                        # rounding the prices to 3 decimals to save characters on twitter
-                        price.append(str(price_rounded))                                                # storing the rounded prices into an array
-                        percent_change_rounded = round(float(value_1['USD']['percent_change_24h']), 2)  # rounding values to 3 decimals as done above
-                        percent_change.append('('+str(percent_change_rounded) + '%)')                   # storing Percent change in 24 hours into an array
+                        price_rounded = round(float(value_1['USD']['price']), 3)                        #rounding the prices to 3 decimals to save characters on twitter
+                        price.append(str(price_rounded))                                          #storing the rounded prices into an array
+                        percent_change_rounded = round(float(value_1['USD']['percent_change_24h']), 2)  #rounding values to 3 decimals as done above
+                        percent_change.append('('+str(percent_change_rounded) + '%)')                        #storing Percent change in 24 hours into an array
                     if key_1 == 'symbol':
                         symbol = '$' + value_1
-                        symbols.append(symbol)                                                          # Cryptocurrency ticker symbol/acronym
-               
+                        symbols.append(symbol) #Cryptocurrency ticker symbol/acronym
+                
 
 except (ConnectionError, Timeout, TooManyRedirects) as e:
     print(e)
   
 pricedata = []
 for i in range(len(symbols)):
-    pricedata.append(str(symbols[i] +': '+ str(price[i]) +' '+ percent_change[i]+'\n'))
+	if 'USD' in symbols[i]:
+		continue
+	else:
+	    pricedata.append(str(symbols[i] +': '+ str(price[i]) +' '+ percent_change[i]+'\n'))
+
+final_pricedata = ''.join([item for item in pricedata])
+final_pricedata = (final_pricedata[:277] + '...') if len(final_pricedata) > 280 else final_pricedata
 
 final_pricedata = (pricedata[0] + 
                   pricedata[1] +
@@ -56,12 +62,15 @@ final_pricedata = (pricedata[0] +
                   pricedata[9] +
                   pricedata[10])
 
-consumer_key = ''                                                           # info from developer.twitter.com
-consumer_secret = ''                                                        # info from developer.twitter.com
-access_token = ''                                                           # info from developer.twitter.com
-access_token_secret = ''                                                    # info from developer.twitter.com
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
-   
+
+
 api.update_status(final_pricedata)
+
+#print(final_pricedata)
